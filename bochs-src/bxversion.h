f52e6638113f775daad8abf40e118928954056d8
kkGh0st <lihao19980218@gmail.com>
2021-07-11 21:13:58 +0800

    the first commit

---
+++ bochs-src/bxversion.h
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////////////////////////////
+// This file is checked in as bxversion.h.in.  The configure script
+// substitutes variables and creates bxversion.h.
+/////////////////////////////////////////////////////////////////////////
+
+#define VERSION       "2.6.11"
+#define VER_SVNFLAG   0
+#define REL_STRING    "Built from SVN snapshot on January 5, 2020"
+#define REL_TIMESTAMP "Sun Jan  5 08:36:00 CET 2020"
